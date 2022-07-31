45a0c1b04f7b8e9a448014163313a08770c4fe22
DESKTOP-4M0GM2N\Nicodemus <nicodemus.python.developer@gmail.com>
2022-08-01 00:34:07 +0800

    [feature/polls] : Part 7 : customizing admin - customizing forms - List of questions - Management of choices - Overriding templates

--- TestProject1/settings.py
+++ TestProject1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TestProject1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
